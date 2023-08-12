5b81c2a22f148210c975a9fbc7905ddb9f3ddbff
mihee <algml9603@gmail.com>
2023-08-12 23:23:45 +0900

    open file

---
+++ src/main/kotlin/com/example/demo/GoToFileAction.kt
@@ -0,0 +1,25 @@
+package com.example.demo
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import java.io.File
+
+class GoToFileAction:AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project: Project? = e.project
+        if (project != null) {
+            val filePath = ""
+            val file = project.baseDir?.findFileByRelativePath(filePath)
+            if (file != null) {
+                FileEditorManager.getInstance(project).openFile(file, true)
+            }
+        }
+    }
+}
+
